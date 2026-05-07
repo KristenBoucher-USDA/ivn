6d9195f425aafa9b1fea96112aaad4ef5446d8d3
Dr. Kristen Boucher <kristen.boucher@usda.gov>
2026-05-07 09:53:15 -0400

    Checker

---
+++ check_import.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append(r'c:\Users\Kristen.Boucher\OneDrive - USDA\Desktop\Data Analysis\IVN\ivn')
+import ivn_components_error_checker
+print('main exists:', hasattr(ivn_components_error_checker, 'main'))
